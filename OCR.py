ba82e91fc39de337957bd8ea1817e688fb55e15d
Madhia <24397644@sun.ac.za>
2023-01-23 22:34:52 +0200

    OCR code

---
+++ OCR.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import pytesseract
+import numpy as np
+
+filename = '5_python-ocr.jpg'
+img1 = np.array(Image.open(filename))
+text = pytesseract.image_to_string(img1)
+
+print(text)
